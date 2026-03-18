3279d11a80117b2040fc982e66af51763b4519ca
Ondřej Kučera <kucera@deepvision.cz>
2026-03-18 12:14:30 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Arduino development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        arduino-cli = pkgs.arduino-cli;
+        upload-port = "/dev/ttyUSB0";
+        fqbn = "arduino:avr:uno";
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            arduino-cli
+          ];
+
+          shellHook = ''
+            exec zsh 
+          '';
+        };
+      }
+    );
+}
